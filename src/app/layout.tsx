ceb92cab449d5c1b7cbee8c37367175494d9d718
LEEJAESHIN <eoyld1231@naver.com>
2025-12-03 16:24:12 +0900

    feat: Pretendard 전체설정, favicon 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,15 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Pretendard:wght@400;500;600;700;800&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body className="bg-white text-gray-900 font-pretendard antialiased">
+      <body className="bg-white text-gray-900 font-sans antialiased">
         <Header />
         {children}
         <Footer />
